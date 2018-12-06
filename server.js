8117c5786693fb88d8e3954a8b47b51c94cedc26
Deziree Teague <dez.teague@gmail.com>
2018-12-06 11:20:39 -0800

    added database for location and weather

--- server.js
+++ server.js
@@ -28,45 +28,135 @@ client.on('error', err => console.error(err));
 app.use(cors());
 
 // -------------------------LOCATION-------------------------
+//location constructor - maps model to schema
 //Referencing the data from the json files that will include longitude and latitude
-function Location(query, res) {
+function Location(query, data) {
   this.search_query = query;
-  this.formatted_query = res.body.results[0].formatted_address;
-  this.latitude = res.body.results[0].geometry.location.lat;
-  this.longitude = res.body.results[0].geometry.location.lng;
+  this.formatted_query = data.formatted_address;
+  this.latitude = data.geometry.location.lat;
+  this.longitude = data.geometry.location.lng;
 }
 
-app.get('/location', (getLocation);
+app.get('/location', (findLocation);
 
-// helper function
-// function searchToLatLng(query) {
-//   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`; return superagent
-//   //return superagent.get(url) compare line below
-//     .get(url)
-//     .then(res => {
-//       return new Location(query, res);
-//     })
-//     .catch(error => handleError(error));
-// }
+function findLocation(req, res) {
+    const locationHandler = {
+        query: req.query.data,
+        cacheHit: (results) => {
+            res.send(results.rows[0]);
+        },
+        cacheMiss: () => {
+            Location.fetchlocation(req.query.data)
+            .then (data => res.send(data));
+        },
+    };
+    Location.lookupLocation(locationHandler);
+}
+
+//save location to database
+Location.prototype.save = function () {
+    let SQL = `
+    INSERT INTO locations
+    (search_query,formatted_query,latitude,longitude)
+    VALUES ($1,$2,$3,$4)
+    RETURNING id
+    `;
+    let values = Object.values(this);
+    return client.query(SQL, values);
+}
+
+//fetch location from api and save to DB
+Location.fetchlocation = (query) => {
+    const _URL = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`;
+    return superagent.get(_URL)
+    .then(data => {
+        if (!data.body.results.length) {throw 'No Data';}
+        else {
+            //create and save instance
+            let location = new Location(query, data.body.results[0]);
+            return location.save()
+                .then ( result => {
+                    location.id = result.rows[0].id
+                    return location;
+                })
+            return location;    
+        }
+    });
+};
+
+//find location in the database
+Location.lookupLocation=(handler) => {
+    const SQL = `SELECT * FROM locations WHERE search_query=$1`;
+    const values = [handler.query];
+    return client.query( SQL, values)
+        .then( results => {
+            if (results.rowCount > 0) {
+                handler.cacheHit(results);
+            }   
+            else {
+                handler.cacheMiss();
+            }
+        })
+        .catch( console.error );
+};
 
 // -------------------------WEATHER-------------------------
+//weather model
 function Weather(day) {
   this.forecast = day.summary;
-  this.time = new Date(day.time * 1000).toDateString();
+  this.time = new Date(day.time * 1000).toString().slice(0, 15);
 }
+
 app.get('/weather', getWeather);
 
 // helper function
 function getWeather(req, res) {
-  const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${req.query.data.latitude},${req.query.data.longitude}`;
-  superagent.get(url)
-    .then(result => {
-      const weatherSummaries = result.body.daily.data.map(day => {
-        return new Weather(day);
+  const handler = {
+    location: req.query.data, 
+    cacheHit: function(result) {
+      res.send(result.rows);
+    },
+    cacheMiss: function() {
+      Weather.fetch(req.query.data)
+        .then( results => res.send(results) )
+        .catch( console.error );
+    },
+  };
+
+  Weather.lookup(handler);
+
+  //save method
+  Weather.prototype.save = function(id) {
+    const SQL = `INSERT INTO weathers (forecast,time,location_id) VALUES ($1,$2,$3);`;
+    const values = Object.values(this);
+    values.push(id);
+    client.query(SQL, values);
+  };
+
+  Weather.lookup = function(handler) {
+    const SQL = `SELECT * FROM weathers WHERE location_id=$1;`;
+    client.query(SQL, [handler.location.id])
+      .then(result => {
+        if (result.rowCount > 0) {
+          handler.cacheHit(result);
+        } else {
+            handler.cacheMiss();
+          }
+        })
+       .catch(error => handleError(error));
+  };
+
+  Weather.fetch = function(locations) {
+    const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${req.query.data.latitude},${req.query.data.longitude}`;
+    return superagent.get(url)
+      .then(result => {
+        const weatherSummaries= result.body.daily.data.map(day =>{
+          summary.save(location.id);
+          return summary;
+        });
+        return weatherSummaries;
       });
-      res.send(weatherSummaries);
-    })
-    .catch(error => handleError(error));
+  };
 }
 
 // the client will recieve an error message upon status error 500
